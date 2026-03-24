9ce8c227bc4f30e45ef56363cf4fbf845071a976
Ayushxsingh100 <Ayushxsingh100@outlook.com>
2026-03-24 09:55:15 +0530

    PHASE 2 HALF DONE

---
+++ src/main/java/com/roomiematch/roomiematchai/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.roomiematch.roomiematchai.controller;
+
+import com.roomiematch.roomiematchai.dto.UserRequestDTO;
+import com.roomiematch.roomiematchai.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody UserRequestDTO request) {
+        return userService.registerUser(request);
+    }
+}
---
+++ src/main/java/com/roomiematch/roomiematchai/dto/UserRequestDTO.java
@@ -0,0 +1,6 @@
+package com.roomiematch.roomiematchai.dto;
+
+public class UserRequestDTO {
+    public String email;
+    public String password;
+}
