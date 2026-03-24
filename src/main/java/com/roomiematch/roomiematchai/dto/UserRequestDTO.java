306082dc08658f40bdcf65e3d4dfdf915c4300c1
Ayushxsingh100 <Ayushxsingh100@outlook.com>
2026-03-24 10:00:39 +0530

    PHASE 2

--- src/main/java/com/roomiematch/roomiematchai/dto/UserRequestDTO.java
+++ src/main/java/com/roomiematch/roomiematchai/dto/UserRequestDTO.java
@@ -1,6 +1,14 @@
 package com.roomiematch.roomiematchai.dto;
 
+import jakarta.validation.constraints.*;
+
+// made a DTO ( client -> controller -> service )
 public class UserRequestDTO {
+
+    @Email
     public String email;
+
+    @NotBlank
+    @Size(min = 4)
     public String password;
 }
